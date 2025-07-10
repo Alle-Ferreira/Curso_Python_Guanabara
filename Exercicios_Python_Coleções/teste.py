ef09490f25cb96d37ab859441838d70376c845f7
Alle-Ferreira <allyferreira@hotmail.com>
2025-07-10 03:17:01 -0300

    Melhorias de Código e Incrementos_090725

--- Exercicios_Python_Coleções/teste.py
+++
@@ -1,9 +0,0 @@
-def calculo_fatorial(numero=1):
-    fator = 1
-    for contador in range(numero, 0, -1):
-        fator *= contador
-    return fator
-
-
-numero = int(input('Insira o numero a ser calculado: '))
-print(f'O fatorial de {numero} é igual a {calculo_fatorial(numero)}')
