1c5279522f5a9bab19d91c03022a7b6177fc0058
Alle-Ferreira <allyferreira@hotmail.com>
2025-07-10 02:48:21 -0300

    Dev dos exercicios 90 a 100.

--- Exercicios_Python_Coleções/teste.py
+++ Exercicios_Python_Coleções/teste.py
@@ -1,9 +1,9 @@
-estados = []
-estado1 = {'uf': 'São Paulo', 'sigla':'SP'}
-estado2 = {'uf': 'Paraná', 'sigla':'PR'}
-estados.append(estado1)
-estados.append(estado2)
+def calculo_fatorial(numero=1):
+    fator = 1
+    for contador in range(numero, 0, -1):
+        fator *= contador
+    return fator
 
-print(estados)
-print(estados[1]) # 
-print()
+
+numero = int(input('Insira o numero a ser calculado: '))
+print(f'O fatorial de {numero} é igual a {calculo_fatorial(numero)}')
